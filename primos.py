df91151acf9fc5fa768d057883bb8af820d08a8a
Zhian <zhiancaballero@gmail.com>
2011-04-06 16:48:55 -0300

    Agrego primos.py

---
+++ primos.py
@@ -0,0 +1,19 @@
+def primos (n):
+    for i in range(2,n):
+        for x in range(2,i):
+            if i % x == 0:               
+                break     
+	else :
+	    print i, 
+
+
+
+
+
+
+
+
+
+
+
+
